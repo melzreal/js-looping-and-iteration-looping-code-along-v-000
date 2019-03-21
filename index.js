2297a67b7c1cfe98a95c4aa5039c238b20f30ce0
Melissa Real <melissa.real@hotmail.com>
2019-03-21 20:31:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function tailsNeverFails(){
 while (tailsNeverFails()) {
     counter = 0;
     counter++;
-    return `You got ${counter} tails in a row!`)
+    return `You got ${counter} tails in a row!`;
 }
